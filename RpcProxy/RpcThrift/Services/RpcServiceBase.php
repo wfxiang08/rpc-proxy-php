259e0e4b2d2777e99b2b745a2dc107d75c943c1c
fei.wang <fei.wang@ushow.media>
2017-07-12 00:27:03 +0800

    添加了Handler实现

--- RpcProxy/RpcThrift/Services/RpcServiceBase.php
+++ RpcProxy/RpcThrift/Services/RpcServiceBase.php
@@ -7,15 +7,19 @@ namespace RpcThrift\Services;
  * DO NOT EDIT UNLESS YOU ARE SURE THAT YOU KNOW WHAT YOU ARE DOING
  * @generated
  */
-use Thrift\Exception\TApplicationException;
-use Thrift\Protocol\TBinaryProtocolAccelerated;
-use Thrift\Type\TMessageType;
+use Thrift\Base\TBase;
 use Thrift\Type\TType;
+use Thrift\Type\TMessageType;
+use Thrift\Exception\TException;
+use Thrift\Exception\TProtocolException;
+use Thrift\Protocol\TProtocol;
+use Thrift\Protocol\TBinaryProtocolAccelerated;
+use Thrift\Exception\TApplicationException;
 
-//
-// 文件名: RpcServiceBase, 内部的类名都是以文件名开头, 例如: RpcServiceBase__If, RpcServiceBase__Client
-//
+
 interface RpcServiceBaseIf {
+  /**
+   */
   public function ping();
 }
 
@@ -38,7 +42,6 @@ class RpcServiceBaseClient implements \RpcThrift\Services\RpcServiceBaseIf {
 
   public function send_ping() {
     $args = new \RpcThrift\Services\RpcServiceBase_ping_args();
-
     $bin_accel = ($this->output_ instanceof TBinaryProtocolAccelerated) && function_exists('thrift_protocol_write_binary');
     if ($bin_accel) {
       thrift_protocol_write_binary($this->output_, 'ping', TMessageType::CALL, $args, $this->seqid_, $this->output_->isStrictWrite());
@@ -52,9 +55,8 @@ class RpcServiceBaseClient implements \RpcThrift\Services\RpcServiceBaseIf {
 
   public function recv_ping() {
     $bin_accel = ($this->input_ instanceof TBinaryProtocolAccelerated) && function_exists('thrift_protocol_read_binary');
-    if ($bin_accel) {
-      $result = thrift_protocol_read_binary($this->input_, '\RpcThrift\Services\RpcServiceBase_ping_result', $this->input_->isStrictRead());
-    } else {
+    if ($bin_accel) $result = thrift_protocol_read_binary($this->input_, '\RpcThrift\Services\RpcServiceBase_ping_result', $this->input_->isStrictRead());
+    else {
       $rseqid = 0;
       $fname = null;
       $mtype = 0;
@@ -168,4 +170,49 @@ class RpcServiceBase_ping_result {
 
 }
 
+class RpcServiceBaseProcessor {
+  protected $handler_ = null;
+
+  public function __construct($handler) {
+    $this->handler_ = $handler;
+  }
+
+  public function process($input, $output) {
+    $rseqid = 0;
+    $fname = null;
+    $mtype = 0;
+
+    $input->readMessageBegin($fname, $mtype, $rseqid);
+    $methodname = 'process_'.$fname;
+    if (!method_exists($this, $methodname)) {
+      $input->skip(TType::STRUCT);
+      $input->readMessageEnd();
+      $x = new TApplicationException('Function '.$fname.' not implemented.', TApplicationException::UNKNOWN_METHOD);
+      $output->writeMessageBegin($fname, TMessageType::EXCEPTION, $rseqid);
+      $x->write($output);
+      $output->writeMessageEnd();
+      $output->getTransport()->flush();
+      return;
+    }
+    $this->$methodname($rseqid, $input, $output);
+    return true;
+  }
+
+  protected function process_ping($seqid, $input, $output) {
+    $args = new \RpcThrift\Services\RpcServiceBase_ping_args();
+    $args->read($input);
+    $input->readMessageEnd();
+    $result = new \RpcThrift\Services\RpcServiceBase_ping_result();
+    $this->handler_->ping();
+    $bin_accel = ($output instanceof TBinaryProtocolAccelerated) && function_exists('thrift_protocol_write_binary');
+    if ($bin_accel) {
+      thrift_protocol_write_binary($output, 'ping', TMessageType::REPLY, $result, $seqid, $output->isStrictWrite());
+    } else {
+      $output->writeMessageBegin('ping', TMessageType::REPLY, $seqid);
+      $result->write($output);
+      $output->writeMessageEnd();
+      $output->getTransport()->flush();
+    }
+  }
+}
 
